8a2271e35ef189629a0722fd8c4e2a7df07261ef
Thandeka325 <marhwayiza325@gmail.com>
2024-11-17 09:57:08 -0800

    Project: 0x00. AirBnB clone - The console

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@ This module defines the BaseModel class, for all the hbnb models.
 """
 
 import models
-import uuid
+from uuid import uuid4
 from datetime import datetime
 
 
@@ -18,18 +18,18 @@ class BaseModel:
                 *args (any): Unused positional arguments.
                 **kwargs (dict): Key/value pairs of attributes.
         """
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         if kwargs:
+            tform = "%Y-%m-%dT%H:%M:%S.%f"
             for key, value in kwargs.items():
-                if key == "__class__":
-                    continue
                 if key in {"created_at", "updated_at"}:
-                    value = datetime.fromisoformat(value)
-                setattr(self, key, value)
+                    setattr(self, key, datetime.strptime(value, tform))
+                elif key is not "__class__":
+                    setattr(self, key, value)
 
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
             models.storage.new(self)
 
     def __str__(self):
